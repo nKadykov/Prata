cbd3b2c0c3a6f5347fa1b675afc6a7f9aa255cba
nKadykov <nkadykov@gmail.com>
2024-03-25 15:49:59 +0300

    init

---
+++ 8/firstref.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    using namespace std;
+    int rats = 101;
+    int &rodents = rats;
+    
+    cout << "rats = " << rats;
+    cout << ", rodents = " << rodents << endl;
+    rodents++;
+    cout << "rats = " << rats;
+    cout << ", rodents = " << rodents << endl;
+
+    cout << "rats address = " << &rats;
+    cout << ", rodents = " << &rodents << endl;
+
+    return 0;
+}
