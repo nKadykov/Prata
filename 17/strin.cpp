c517f305cd64043d5c4a792dd8e352b087b773ef
nKadykov <nkadykov@gmail.com>
2024-04-13 09:41:42 +0300

    init

---
+++ 17/strin.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+
+int main() {
+    using namespace std;
+    string lit = "It was a dark and stormy day, and the full moon glowed brillianly. ";
+    istringstream instr(lit);
+    string word;
+    while(instr >> word) {
+        cout << word << endl;
+    }
+    return 0;
+}
